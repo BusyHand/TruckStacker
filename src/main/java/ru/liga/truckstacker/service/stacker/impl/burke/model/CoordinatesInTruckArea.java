21930fc41aecf9f23803910b887e1a8c4a6a7280
BusyHand <88427170+BusyHand@users.noreply.github.com>
2024-09-22 16:54:55 +0500

    Delete src directory

--- src/main/java/ru/liga/truckstacker/service/stacker/impl/burke/model/CoordinatesInTruckArea.java
+++
@@ -1,7 +0,0 @@
-
-package ru.liga.truckstacker.service.stacker.impl.burke.model;
-/**
- * Record to hold coordinates within the truck area.
- */
-public record CoordinatesInTruckArea(int x, int y) {
-}
