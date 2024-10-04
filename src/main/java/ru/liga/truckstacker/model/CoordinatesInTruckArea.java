bb3747f3227d97739ca21530cc60fbb19d796783
piloww <107499815+Okrill@users.noreply.github.com>
2024-10-04 18:33:26 +0500

    Delete src directory

--- src/main/java/ru/liga/truckstacker/model/CoordinatesInTruckArea.java
+++
@@ -1,8 +0,0 @@
-package ru.liga.truckstacker.model;
-/**
- * Объект, представляющий координаты в области грузовика.
- * @param x Координата по оси X.
- * @param y Координата по оси Y.
- */
-public record CoordinatesInTruckArea(int x, int y) {
-}
